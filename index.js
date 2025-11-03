39e46ae70f51f8544ad67f7efb10cc37117bf6d5
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-03 08:37:42 +0700

    merubah file server index.js dengan menambahkan method post yang benar

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const db = require("./models");
+const buku = require('./models/buku');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,47 +13,12 @@ db.sequelize.sync()
   })
   .catch(err => console.log(err));
 
-app.post("/mahasiswa", async (req, res) => {
+  app.post("/buku", async (req, res) => {
   try {
-    const data = await db.Mahasiswa.create(req.body);
+    const data = await db.Buku.create(req.body);
     res.send(data);
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
-app.get("/mahasiswa", async (req, res) => {
-  try {
-    const mahasiswa = await db.Mahasiswa.findAll();
-    res.send(mahasiswa);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.put("/mahasiswa/:nim", async (req, res) => {
-  const nim = req.params.nim;
-  try {
-    const mahasiswa = await db.Mahasiswa.findByPk(nim);
-    if (!mahasiswa) return res.status(404).send({ message: "Siswa tidak ditemukan" });
-    await mahasiswa.update(req.body);
-    res.send({ message: "Siswa berhasil diupdate", mahasiswa });
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-app.delete("/mahasiswa/:nim", async (req, res) => {
-  const nim = req.params.nim;
-  try {
-    const mahasiswa = await db.Mahasiswa.findByPk(nim);
-    if (!mahasiswa) return res.status(404).send({ message: "Siswa tidak ditemukan" });
-    await mahasiswa.destroy();
-    res.send({ message: "Siswa berhasil dihapus" });
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
